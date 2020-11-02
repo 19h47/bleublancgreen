c51281742652e908f29ed9cbd6a92f3294020be7
19h47 <jeremylevron@19h47.fr>
2020-11-02 19:57:47 +0100

    :sparkles: Carousel section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,16 @@
 /* global Wishlist */
+import Flickity from 'flickity';
+
 require('./wishlist');
 require('./templates');
 
 (() => {
 	Wishlist.init();
+
+	const carousels = [...document.querySelectorAll('.js-carousel')];
+
+	carousels.forEach($carousel => {
+		const $items = $carousel.querySelector('.js-items');
+		const carousel = new Flickity($items, { setGallerySize: false }); // eslint-disable-line no-unused-vars
+	});
 })();
